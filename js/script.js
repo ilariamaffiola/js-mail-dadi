f23d49d992f71e071a471ced557f4a4ecbe27623
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-20 15:25:09 +0100

    [UPD]Add prompt for user email input

--- js/script.js
+++ js/script.js
@@ -2,3 +2,6 @@
 // creare l'array per la lista di email di invitati alla festa
 let mails = ['im02@gmail.com' , 'marcobianchi@libero.it' , 'salvatoreavarzulla@outlook.com' , 'mariorossi@hotmail.com' , 'loremipsum@lorem.it' , 'rebeccalol@libero.it' , 'ciaociao@gmail.com'];
 console.log(mails);
+//dichiarare un prompt per chiedere all'utente la sua email
+let email = prompt('Inserisci la tua email');
+console.log(email);
