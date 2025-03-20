ec095a360751916c2a0ecd9b8aed86733c960870
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-20 15:24:38 +0100

    [UPD]Add email list for party invitations

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//MAIL
+// creare l'array per la lista di email di invitati alla festa
+let mails = ['im02@gmail.com' , 'marcobianchi@libero.it' , 'salvatoreavarzulla@outlook.com' , 'mariorossi@hotmail.com' , 'loremipsum@lorem.it' , 'rebeccalol@libero.it' , 'ciaociao@gmail.com'];
+console.log(mails);
