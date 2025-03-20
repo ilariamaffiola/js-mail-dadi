2d319449a9a5f020285153151fe382eb93c51d90
ilariamaffiola <maffiolailaria1@gmail.com>
2025-03-20 15:49:37 +0100

    [UPD]ultimo cambiamento per il ticket

--- js/script.js
+++ js/script.js
@@ -20,3 +20,26 @@ else{
     console.log('Non sei stato invitato alla festa');
 }
 
+//GIOCO DEI DADI
+//immagino che ci siano due giocatori che lanciano un dado a testa
+let player1= 'Ciccio';
+let player2= 'pc';
+console.log(player1);
+console.log(player2);
+//essendoci due giocatori devo creare due variabili per i due dadi (cioè i due giocatori)
+//creo la variabile del dado del giocatore 1
+let dado1;
+//creo la variabile del dado del giocatore 2
+let dado2;
+//dovrei mettere un array con numeri da 1 a 6 per simulare il lancio del dado
+let diceNumbers = [1, 2, 3, 4, 5, 6];
+console.log(diceNumbers);
+//dovrei ciclare l'array per generare un numero casuale tra 1 e 6
+for(let i=0; i<diceNumbers.length; i++){
+    
+}
+//creo un if per stabilire chi è il vincitore
+//creo un alert per comunicare il vincitore
+//creo un alert per comunicare il risultato del lancio dei due dadi
+
+
